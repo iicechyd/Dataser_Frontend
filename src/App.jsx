d42e7ba5976d82e70a9195485e89802b98d9ca2d
JutathipPraw <jutathip.sr@kkumail.com>
2024-09-10 18:39:11 +0700

    add file

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Im
 import './App.css';
 import Login from './pages/Login';  // Import Login from pages
 import DefaultLayout from './layout/default';
-
+import Student from './pages/Student';
+import Teacher from './pages/Teacher';
 
 function App() {
   return (
@@ -12,7 +13,9 @@ function App() {
       <Routes>
         {/* กำหนดเส้นทางให้ไปยังหน้า Login */}
         <Route path="/" element={<Login />} />
-        
+        <Route path="/" element={<Login />} />
+        <Route path="/students" element={isLoggedIn ? <Student/> : <Navigate to="/login" />} />
+        <Route path="/teachers" element={isLoggedIn ? <Teacher /> : <Navigate to="/login" />} />
       </Routes>
       </DefaultLayout>
     </Router>
