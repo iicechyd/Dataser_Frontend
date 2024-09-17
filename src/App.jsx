3d4c5f165379343f726a9bbb33a8f27c0fb50e7f
iicechyd <chayada.w@kkumail.com>
2024-09-17 16:33:44 +0700

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; 
-import './App.css';
 import Login from './pages/Login';  
 import StudentList from './pages/Student';
 import TeacherList from './pages/Teacher';
@@ -8,6 +7,11 @@ import Coursedetail from './pages/Coursedetail';
 import ShowstudentList from './pages/studentlist';
 
 import DefaultLayout from './layout/default';
+import StdList from './pages/StdList';
+import AttenStat from './pages/AttenStat';
+import CheckIn from './pages/checkin'
+import CourseList from './pages/CourseList'
+import TeacherAddCourse  from './pages/TeacherAddCourse';
 
 function App() {
   const token = localStorage.getItem('token'); 
@@ -22,6 +26,12 @@ function App() {
         <Route path="/detailteachers" element={isLoggedIn ? <Coursedetail /> : <Navigate to="/" />} />
         <Route path="/Studentlist" element={isLoggedIn ? <ShowstudentList /> : <Navigate to="/" />} />
 
+        <Route path="/StdList" element={isLoggedIn ? <StdList/> : <Navigate to="/" />} />
+        <Route path="/AttenStat" element={isLoggedIn ? <AttenStat/> : <Navigate to="/" />} />
+        <Route path="/checkin/:course_code" element={<CheckIn />} />
+        <Route path="/CourseList/" element={<CourseList />} />
+        <Route path="/checkinstatus/:course_code" element={<StdList/>} />
+        <Route path="/TeacherAddCourse/" element={<TeacherAddCourse/>} />
       </Routes>
     </Router>
   );
