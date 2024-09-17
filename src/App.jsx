74495397bca935526d88f02324cbeff95be6fca7
iicechyd <chayada.w@kkumail.com>
2024-09-17 16:18:04 +0700

    Reapply "edit"
    This reverts commit bf637d896c41463103fe26340f41ecb58bab774d.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './App.css';
 import Login from './pages/Login';  
 import StudentList from './pages/Student';
 import TeacherList from './pages/Teacher';
+import Coursedetail from './pages/Coursedetail';
+import ShowstudentList from './pages/studentlist';
 
 import DefaultLayout from './layout/default';
 
@@ -17,6 +19,9 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/students" element={isLoggedIn ? <StudentList /> : <Navigate to="/" />} />
         <Route path="/teachers" element={isLoggedIn ? <TeacherList /> : <Navigate to="/" />} />
+        <Route path="/detailteachers" element={isLoggedIn ? <Coursedetail /> : <Navigate to="/" />} />
+        <Route path="/Studentlist" element={isLoggedIn ? <ShowstudentList /> : <Navigate to="/" />} />
+
       </Routes>
     </Router>
   );
