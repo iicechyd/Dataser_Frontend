bf637d896c41463103fe26340f41ecb58bab774d
iicechyd <chayada.w@kkumail.com>
2024-09-17 16:17:23 +0700

    Revert "edit"
    This reverts commit 43eaa1d20b7af9b025ddf724b12ff516725b06fd.

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import './App.css';
 import Login from './pages/Login';  
 import StudentList from './pages/Student';
 import TeacherList from './pages/Teacher';
-import Coursedetail from './pages/Coursedetail';
-import ShowstudentList from './pages/studentlist';
 
 import DefaultLayout from './layout/default';
 
@@ -19,9 +17,6 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/students" element={isLoggedIn ? <StudentList /> : <Navigate to="/" />} />
         <Route path="/teachers" element={isLoggedIn ? <TeacherList /> : <Navigate to="/" />} />
-        <Route path="/detailteachers" element={isLoggedIn ? <Coursedetail /> : <Navigate to="/" />} />
-        <Route path="/Studentlist" element={isLoggedIn ? <ShowstudentList /> : <Navigate to="/" />} />
-
       </Routes>
     </Router>
   );
